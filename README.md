71d33783985fe070385db7318c8d79d5fc01d307
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2014-01-06 17:17:00 -0500

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ touch
 =====
 
 like unix touch
+change a file's timestamp, or create a file
+
+Build
+
+- Use go build tools.
+
+$ go build touch.go
+or
+$ go install
+
+Cross-compile
+
+$ gox -output="touch-{{.OS}}-{{.Arch}}"
+
+TODO
+
+- implement all the unix flags
---
+++ touch.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+    "time"
+)
+
+func touch(path string) {
+	if _, err := os.Stat(path); err != nil {
+        if os.IsNotExist(err) {
+            if _, err := os.Create(path); err != nil {
+                log.Fatal(err)
+            }
+        } else {
+            log.Fatal(err)
+        }
+	} else {
+        now := time.Now()
+        os.Chtimes(path,now,now)
+	}
+}
+
+func main() {
+	flag.Parse()
+	for _, path := range flag.Args() {
+		touch(path)
+	}
+}
